30b9e243cf78f05b9995120e7848652c43b287c6
Dhananjay Patel <dhananjaypatel.official@gmail.com>
2022-08-08 13:09:09 +0000

    Demo

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,49 +2,58 @@ const express = require("express");
 const route = express.Router();
 const session = require("express-session")
 const passport = require("passport");
-const { initialize } = require("passport/lib");
-const GitHubStrategy = require('passport-github2').Strategy
 const authController = require("../../controller/auth")
+// const passport = require("passport");
+const GitHubStrategy = require('passport-github2').Strategy
 
-route.use(
-    session({
-        secret:'keyboard dash dark',
-        resave: false,
-        saveUninitialized: false,
-        cookie:{ httpOnly: true , secure: false , maxAge: 7*24*60*60*1000}
+const CLI_URL = "https://qkblch-3000.preview.csb.app/dashboard"
+
+// route.get("/login/success", (req, res) => {
+//   if (req.user) {
+//     res.status(200).json({
+//       success: true,
+//       message: "successfull",
+//       user: req.user,
+//       //   cookies: req.cookies
+//     });
+//   }
+// });
+route.get("/login/success",authController.githubAuth);
+
+route.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failure",
+  });
+});
+
+route.get("/logout", (req, res) => {
+    res.json({
+        status : "failed"
     })
+});
+
+
+route.get("/github", passport.authenticate("github", { scope: ["profile"] }));
+
+route.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    successRedirect: CLI_URL,
+    failureRedirect: "/login/failed",
+  })
 );
 
-route.use(passport.initialize());
-route.use(passport.session());
+// route.get("/github/success",authController.githubAuth)
 
-passport.serializeUser(function(user,done){
-    done(null,user._json)
-})
-passport.deserializeUser(function(id,done){
-    done(null,id)
-})
-passport.use(new GitHubStrategy({
-        clientID: "df5ae050a0da7df02ae8",
-        clientSecret: "6a786c90864cc53b8e5f0225da261a72b5dedcf2",
-        callbackURL: "https://zwvqkv-3333.preview.csb.app/auth/github/callback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-        done(null,profile)
-    }
-));
+// route.get('/github',
+// passport.authenticate('github', { scope: [ 'user:email' ] }));
 
-route.get("/github/success",authController.githubAuth)
 
-route.get('/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }));
+// route.get('/github/callback', 
+// passport.authenticate('github', { failureRedirect: '/login/failed',successRedirect : CLI_URL }));
 
-route.get('/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login',successRedirect : '/auth/github/success' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+
 
 module.exports = route;
 
